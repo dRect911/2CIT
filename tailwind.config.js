03dbe4095a21f7096241eab3c3cdd6d412143506
dRect911 <94997838+dRect911@users.noreply.github.com>
2023-02-28 21:06:42 +0000

    style: added theme colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:{
+          DEFAULT:'#0E7DEC',
+          light:'#C2DAF2',
+          dark:'#2673BF',
+        },
+        secondary:{
+          DEFAULT:'#EC7C0E',
+          light:'#F2DAC2',
+          dark:'#BF7326', 
+        },
+        dark:'#191919',
+        light:'#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
