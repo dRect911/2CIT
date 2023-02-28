a5dfcd3af4a68090fec93104f3c958a4cd8223ac
dRect911 <94997838+dRect911@users.noreply.github.com>
2023-02-28 22:37:39 +0000

    style: added logo to the navbar (placeholder file)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,11 @@ import { Search } from '@carbon/icons-react';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full p-3 flex justify-end bg-transparent px-[240px] backdrop-blur-md z-50">
+    <div className="fixed top-0 w-full p-3 flex justify-between bg-transparent px-[240px] backdrop-blur-md z-50">
+        {/* <div className="rounded-full p-[1px] " >
+
+        </div> */}
+        <img src="/logo-2cit.png" className="h-12 rounded-full" />
         <ul className="flex gap-6 items-center font-normal text-[18px] " >
             <li className="text-primary hover:text-primary-dark transition-all cursor-pointer " ><a href="#home">Accueil</a></li>
             <li><a href="#section1">Section 1</a></li>
