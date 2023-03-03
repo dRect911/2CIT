e923d510035ad991b75c5cf2629f067316247dea
dRect911 <94997838+dRect911@users.noreply.github.com>
2023-03-03 16:16:26 +0000

    feat: updated meta description | no image yet

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+// import Head from 'next/head'
 import Navbar from '@/components/Navbar'
 import { useState, useEffect } from 'react';
 
@@ -13,7 +14,9 @@ export default function Document() {
 
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <meta name="description" content="Découvrez le Centre de Conception et d'Innovation Technologique. 2CIT dispose d'un personnel composé d'ingénieurs et de techniciens ayant fait leurs preuves dans diverses entreprises de la place." />
+      </Head>
       <body className="w-full relative" >
         <Navbar currentSection={currentSection} setCurrentSection={setCurrentSection} />
         <Main />
